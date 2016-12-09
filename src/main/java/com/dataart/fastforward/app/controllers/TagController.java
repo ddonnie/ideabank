7232478aa59c0c40b69682deaf8797dd3f5aea37
ddonnie <mohax91@gmail.com>
2016-12-09 23:39:36 +0200

    implement api */tags/{tagName} for search by tag name

--- src/main/java/com/dataart/fastforward/app/controllers/TagController.java
+++ src/main/java/com/dataart/fastforward/app/controllers/TagController.java
@@ -1,16 +1,17 @@
 package com.dataart.fastforward.app.controllers;
 
 import com.dataart.fastforward.app.dao.TagRepository;
+import com.dataart.fastforward.app.model.Idea;
 import com.dataart.fastforward.app.model.Tag;
 import com.dataart.fastforward.app.services.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.Lifecycle;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by logariett on 29.11.2016.
@@ -30,8 +31,8 @@ public class TagController {
         return tagService.getAll();
     }
 
-    @GetMapping(value = "/{tagId}")
-    public Tag getTagById(@PathVariable long tagId) {
-        return tagService.getTagById(tagId);
+    @GetMapping(value = "/{tagName}")
+    public Set<Idea> getTagByName(@PathVariable String tagName) {
+        return tagService.getTagByTagName(tagName).getIdeasWithThisTag();
     }
 }
